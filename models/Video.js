73f61ca4674460cde6ae8dd4a75073573adf5248
universla <ramirezrodriguezirma292@gmail.com>
2025-07-21 04:46:30 -0600

    Create Video.js

---
+++ models/Video.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Video", videoSchema);
